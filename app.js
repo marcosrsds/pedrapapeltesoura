afe9d77db422698b5815b97ba3332285320854b5
Marcos Roberto <marcosrsoaress@gmail.com>
2025-02-26 20:52:36 -0300

    colocado opacidade nas imagens

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-function escolhaPedra() {
-    console.log("botão PEDRA funcionando");
-    escolha();
-}
-
-function escolhaPapel() {
-    console.log("botão PAPEL funcionando");
-    escolha();
-}
-
-function escolhaTesoura() {
-    console.log("botão TESOURA funcionando");
-    escolha();
-}
-
 function escolha() {
     let escolhaAi = parseInt(Math.random() * 3);
     if (escolhaAi == 0) {
-    console.log("computador escolheu PEDRA");
+        let escolhaPc = 0;
+        console.log("PC escolheu PEDRA");
     } else if (escolhaAi == 1) {
-        console.log("computador escolheu PAPEL");
+        let escolhaPc = 1;
+        console.log("PC escolheu PAPEL");
     } else {
-        console.log("computador escolheu TESOURA");
+        let escolhaPc = 2; 
+        console.log("PC escolheu TESOURA");
     }     
+}
+
+function escolhaPedra(escolhaPc) {
+    console.log("você escolheu PEDRA");
+    escolha();
+    if (escolhaPc == 2) {
+        console.log("você ganhou");
+    } else if (escolhaPc == 1) {
+        console.log("você perdeu");
+    } else {
+        console.log("empate, jogue outra vez...");
+    }
+}
+
+function escolhaPapel(escolhaAi) {
+    console.log("você escolheu PAPEL");
+    escolha();
+    if (escolhaAi == 0) {
+        console.log("você ganhou");
+    } else if (escolhaAi == 2) {
+        console.log("você perdeu");
+    } else if (escolhaAi == 1) {
+        console.log("empate, jogue outra vez...");
+    }
+}
+
+function escolhaTesoura(escolhaAi) {
+    console.log("você escolheu TESOURA");
+    escolha();
+    if (escolhaAi == 1) {
+        console.log("você ganhou");
+    } else if (escolhaAi == 2) {
+        console.log("você perdeu");
+    } else if (escolhaAi == 0) {
+        console.log("empate, jogue outra vez...");
+    }
 }
